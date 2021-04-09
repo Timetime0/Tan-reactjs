ccc36507d1aa3e6dc4de36085cac78e0487a1d3e
Tan <hoangtan.nguyen1007@gmail.com>
2021-04-09 20:50:52 +0700

    add code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import BaiTap1 from "./components/BaiTap1/BaiTap1";
+
+function App() {
+  return (
+    <div>
+      <BaiTap1/>
+
+    </div>
+  );
+}
+
+export default App;
+
+//jsx: react app sử dụng tool jsx để viết (có thể vieert js và html)
